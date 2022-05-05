521a7ef054fe6a4972f1c0c10f510b03541408c6
Daniel Buaon <dbuaon@gmail.com>
2022-05-04 23:32:04 -0300

    Initial commit

---
+++ textoAnimadoConcurrente.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+import time
+import threading
+
+
+main_window = tk.Tk()
+main_window.title("Ejemplo")
+main_window.configure(width=400, height=200)
+
+def opcionFinal():
+    boton = ttk.Button(main_window, text="Salir", command=main_window.destroy)
+    boton.place(x=170, y=170)
+
+def createLabel(a,b):
+    label = ttk.Label(text="")
+    label.place(x=a,y=b)
+    return label
+
+def crearAnimacion(a, b, char):
+    mylabel = createLabel(a=a,b=b)
+    texto=""
+    retardo: float=0.25
+    for i in range(0,35):
+        time.sleep(retardo)
+        texto += char
+        mylabel.config(text = texto)
+        main_window.update()
+
+t1 = threading.Thread(target= crearAnimacion, args=(0, 10, 'X'))
+t2 = threading.Thread(target= crearAnimacion, args=(0, 30, 'Y'))
+t3 = threading.Thread(target= crearAnimacion, args=(0, 50, 'Z'))
+
+t1.start()
+t2.start()
+t3.start()
+
+opcionFinal()
+main_window.mainloop()
